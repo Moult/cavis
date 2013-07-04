290da5fd831384b836744d7d45e47200024fd092
Dion Moult <dion@thinkmoult.com>
2013-07-05 02:30:17 +1000

    Add user data type

---
+++ spec/Cavis/Core/Data/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace spec\Cavis\Core\Data;
+
+use PHPSpec2\ObjectBehavior;
+
+class User extends ObjectBehavior
+{
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Cavis\Core\Data\User');
+    }
+
+    function it_should_have_an_ip()
+    {
+        $this->ip->shouldBe(NULL);
+    }
+}
---
+++ src/Cavis/Core/Data/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Cavis\Core\Data;
+
+class User
+{
+    public $ip;
+}
