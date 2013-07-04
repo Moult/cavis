d01d79123a101d454b26afd7139f3276de6037ba
Dion Moult <dion@thinkmoult.com>
2013-07-04 23:10:16 +1000

    Define validation behavior in image add submission role

--- spec/Cavis/Core/Usecase/Image/Add/Submission.php
+++ spec/Cavis/Core/Usecase/Image/Add/Submission.php
@@ -6,8 +6,49 @@ use PHPSpec2\ObjectBehavior;
 
 class Submission extends ObjectBehavior
 {
+    /**
+     * @param Cavis\Core\Data\Image $image
+     * @param Cavis\Core\Tool\Validation $validation
+     */
+    function let($image, $validation)
+    {
+        $image->name = 'Foo';
+        $image->file = 'file';
+        $this->beConstructedWith($image, $validation);
+        $this->name->shouldBe('Foo');
+        $this->file->shouldBe('file');
+    }
+
     function it_should_be_initializable()
     {
         $this->shouldHaveType('Cavis\Core\Usecase\Image\Add\Submission');
+    }
+
+    function it_should_be_an_image()
+    {
+        $this->shouldHaveType('Cavis\Core\Data\Image');
+    }
+
+    function it_should_validate_the_submission($validation)
+    {
+        $validation->setup(array(
+            'name' => 'Foo',
+            'file' => 'file',
+        ))->shouldBeCalled();
+        $validation->rule('name', 'not_empty')->shouldBeCalled();
+        $validation->rule('name', 'max_length', 30)->shouldBeCalled();
+        $validation->rule('file', 'upload_valid')->shouldBeCalled();
+        $validation->rule('file', 'upload_type', array('jpg', 'png', 'jpeg'))->shouldBeCalled();
+        $validation->rule('file', 'upload_size', '1M')->shouldBeCalled();
+        $validation->check()->shouldBeCalled()->willReturn(TRUE);
+        $this->validate();
+    }
+
+    function it_should_throw_an_error_at_invalid_submissions($validation)
+    {
+        $validation->check()->shouldBeCalled()->willReturn(FALSE);
+        $validation->errors()->shouldBeCalled()->willReturn(array('name', 'file'));
+        $this->shouldThrow('Cavis\Core\Exception\Validation')
+            ->duringValidate();
     }
 }
--- src/Cavis/Core/Usecase/Image/Add/Submission.php
+++ src/Cavis/Core/Usecase/Image/Add/Submission.php
@@ -1,7 +1,35 @@
 <?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
 
 namespace Cavis\Core\Usecase\Image\Add;
+use Cavis\Core\Data;
+use Cavis\Core\Tool;
+use Cavis\Core\Exception;
 
-class Submission
+class Submission extends Data\Image
 {
+    public function __construct(Data\Image $image, Tool\Validation $validation)
+    {
+        $this->name = $image->name;
+        $this->file = $image->file;
+        $this->validation = $validation;
+    }
+
+    public function validate()
+    {
+        $this->validation->setup(array(
+            'name' => $this->name,
+            'file' => $this->file,
+        ));
+        $this->validation->rule('name', 'not_empty');
+        $this->validation->rule('name', 'max_length', 30);
+        $this->validation->rule('file', 'upload_valid');
+        $this->validation->rule('file', 'upload_type', array('jpg', 'png', 'jpeg'));
+        $this->validation->rule('file', 'upload_size', '1M');
+        if ( ! $this->validation->check())
+            throw new Exception\Validation($this->validation->errors());
+    }
 }
