42fca32fc8113fca836292e30e0f42a09d529e9b
Dion Moult <dion@thinkmoult.com>
2013-07-04 23:09:42 +1000

    Create validation exception type

---
+++ spec/Cavis/Core/Exception/Validation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace spec\Cavis\Core\Exception;
+
+use PHPSpec2\ObjectBehavior;
+
+class Validation extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(array('foo' => 'bar'));
+    }
+
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Cavis\Core\Exception\Validation');
+    }
+
+    function it_is_an_exception()
+    {
+        $this->shouldHaveType('Exception');
+    }
+
+    function it_should_return_errors_as_an_array()
+    {
+        $this->get_errors()->shouldBe(array('foo' => 'bar'));
+    }
+}
---
+++ src/Cavis/Core/Exception/Validation.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Cavis\Core\Exception;
+
+class Validation extends \Exception
+{
+    private $errors = array();
+
+    public function __construct(array $errors)
+    {
+        $this->errors = $errors;
+    }
+
+    public function get_errors()
+    {
+        return $this->errors;
+    }
+}
