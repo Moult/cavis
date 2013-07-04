9168b486bb4ee9ca51f3f1e235b81dfc9839b353
Dion Moult <dion@thinkmoult.com>
2013-07-04 23:09:16 +1000

    Specify image data type

---
+++ spec/Cavis/Core/Data/Image.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace spec\Cavis\Core\Data;
+
+use PHPSpec2\ObjectBehavior;
+
+class Image extends ObjectBehavior
+{
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Cavis\Core\Data\Image');
+    }
+
+    function it_should_have_a_name()
+    {
+        $this->name->shouldBe(NULL);
+    }
+
+    function it_should_have_file_data()
+    {
+        $this->file->shouldBe(NULL);
+    }
+}
---
+++ src/Cavis/Core/Data/Image.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Cavis\Core\Data;
+
+class Image
+{
+    public $name;
+    public $file;
+}
