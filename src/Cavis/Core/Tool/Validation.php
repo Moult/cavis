1f04c0a1345d103a34970855ae137c7e044fa63b
Dion Moult <dion@thinkmoult.com>
2013-07-04 23:09:52 +1000

    Define validation tool

---
+++ src/Cavis/Core/Tool/Validation.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Cavis\Core\Tool;
+
+interface Validation
+{
+    /**
+     * Loads in the input data that you want to perform validation checks on.
+     *
+     * Example:
+     * $validation = new Validation_Class;
+     * $validation->setup($_POST);
+     *
+     * @param array $input_data
+     * @return void
+     */
+    public function setup(array $input_data);
+
+    /**
+     * Adds a rule for checking the input with key $key using a predefined rule.
+     *
+     * Example:
+     * $validation->rule('username', 'not_empty');
+     *
+     * @param string $key  The key to access the input data value
+     * @param string $rule not_empty - passes if there is a value
+     *                     max_length - passes if chars are less than $arg
+     *                     ip - passes if valid ip address
+     *                     upload_valid - passes if upload data is valid
+     *                     upload_type - passes if upload data fits filetypes in $arg array
+     *                     upload_size - passes if upload data is less than size in $arg string (eg: 1M, 2KiB, 1GB)
+     * @param string $arg  Any extra arguments related to the rule
+     * @return void
+     */
+    public function rule($key, $rule, $arg = NULL);
+
+    /**
+     * Adds a custom function to validate the input with key $key.
+     *
+     * Example:
+     * // will call $this->is_existing_account();
+     * $validation->callback($this, 'is_existing_account');
+     *
+     * @param string $key      The key to access the input data value
+     * @param array  $function Array($object, string $function_name) which has a
+     *                         return type of bool
+     * @param array  $args     An array of setup keys to be used as arguments in
+     *                         the callback function
+     * @return void
+     */
+    public function callback($key, array $function, array $args);
+
+    /**
+     * Runs all of the added rules and callbacks, logging whether or not there
+     * are any validation errors.
+     *
+     * Example:
+     * if ($validation->check() === TRUE) {} // All items are valid.
+     *
+     * @return bool
+     */
+    public function check();
+
+    /**
+     * Returns an array with any errors found.
+     *
+     * Example:
+     * print_r($validation->check()); // return array('keyfoo', 'keybar')
+     *
+     * @return array
+     */
+    public function errors();
+}
