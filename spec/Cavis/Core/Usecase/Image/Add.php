4f4d1ff869ad3f46d328fe0f0b4b9f5f0a139269
Dion Moult <dion@thinkmoult.com>
2013-07-05 01:09:42 +1000

    Add DI resolver for image add usecase

---
+++ spec/Cavis/Core/Usecase/Image/Add.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace spec\Cavis\Core\Usecase\Image;
+
+use PHPSpec2\ObjectBehavior;
+
+class Add extends ObjectBehavior
+{
+    /**
+     * @param Cavis\Core\Data\Image $image
+     * @param Cavis\Core\Usecase\Image\Add\Repository $image_add
+     * @param Cavis\Core\Tool\Graphic $graphic
+     * @param Cavis\Core\Tool\Filesystem $filesystem
+     * @param Cavis\Core\Tool\Validation $validation
+     */
+    function let($image, $image_add, $graphic, $filesystem, $validation)
+    {
+        $data = array(
+            'image' => $image
+        );
+
+        $repositories = array(
+            'image_add' => $image_add
+        );
+
+        $tools = array(
+            'graphic' => $graphic,
+            'filesystem' => $filesystem,
+            'validation' => $validation
+        );
+
+        $this->beConstructedWith($data, $repositories, $tools);
+    }
+
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Cavis\Core\Usecase\Image\Add');
+    }
+
+    function it_should_fetch_the_interactor()
+    {
+        $this->fetch();
+    }
+}
---
+++ src/Cavis/Core/Usecase/Image/Add.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Cavis\Core\Usecase\Image;
+use Cavis\Core\Usecase\Image\Add\Interactor;
+use Cavis\Core\Usecase\Image\Add\Submission;
+
+class Add
+{
+    private $data;
+    private $repositories;
+    private $tools;
+
+    public function __construct(Array $data, Array $repositories, Array $tools)
+    {
+        $this->data = $data;
+        $this->repositories = $repositories;
+        $this->tools = $tools;
+    }
+
+    public function fetch()
+    {
+        return new Interactor(
+            $this->get_submission()
+        );
+    }
+
+    private function get_submission()
+    {
+        return new Submission(
+            $this->data['image'],
+            $this->repositories['image_add'],
+            $this->tools['graphic'],
+            $this->tools['filesystem'],
+            $this->tools['validation']
+        );
+    }
+}
