4f1353b00430db9ab60522aa9337842ea0798193
Dion Moult <dion@thinkmoult.com>
2013-07-04 23:39:52 +1000

    Add file data type

---
+++ src/Cavis/Core/Data/File.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Cavis\Core\Data;
+
+class File
+{
+    public $name;
+    public $tmp_name;
+    public $mimetype;
+    public $filesize_in_bytes;
+    public $error_code;
+}
